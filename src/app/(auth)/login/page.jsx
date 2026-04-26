c039fb50341b18fc32dd0a22dd86a4f823813700
Mezba Morsalin <mejbahmorsalin560@hotmail.com>
2026-04-27 02:35:14 +0600

    first commit

---
+++ src/app/(auth)/login/page.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const LoginPage = () => {
+    return (
+        <div className='max-w-3xl mx-auto'>
+            <form className="fieldset bg-base-200 border-base-300 rounded-box  border p-4 flex flex-col justify-center">
+                <h2 className='text-4xl font-bold'>Login your account</h2>
+  <label className="label">Email Address</label>
+  <input type="email" className="input" placeholder="Email" />
+
+  <label className="label">Password</label>
+  <input type="password" className="input" placeholder="Password" />
+
+  <button className="btn btn-neutral mt-4 w-80">Login</button>
+</form>
+        </div>
+    );
+};
+
+export default LoginPage;
---
+++ src/app/(main)/page.js
@@ -0,0 +1,10 @@
+
+
+export default function Home() {
+  return (
+    <>
+    <h2 className="text-2xl text-indigo-400">Home</h2>
+    <button className="btn btn-primary">Here</button>
+    </>
+  );
+}
