224ce622a7dbc8599dcf9108b75f6771969acf46
Mezba Morsalin <mejbahmorsalin560@hotmail.com>
2026-04-27 02:45:23 +0600

    Header Completed and add date fns

--- src/components/shared/Header.jsx
+++ src/components/shared/Header.jsx
@@ -1,9 +1,13 @@
 import React from 'react';
-
+import logo from '@/assets/logo.png'
+import Image from 'next/image';
+import { format } from 'date-fns';
 const Header = () => {
     return (
-        <div>
-            Header
+        <div className='text-center space-y-2 py-8'>
+            <Image className='mx-auto' src={logo} width={300} height={300} alt='logo'></Image>
+            <p className='text-[#706F6F] text-lg'>Journalism Without Fear or Favour</p>
+            <p>{format(new Date(), "EEEE, MMMM dd, yyyy")}</p>
         </div>
     );
 };
