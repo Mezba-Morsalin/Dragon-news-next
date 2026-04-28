d225f7121a0e1d8ac24ca6ccc262642fded14bd1
Mezba Morsalin <mejbahmorsalin560@hotmail.com>
2026-04-29 03:47:57 +0600

    Login and Register Page Completed

--- src/app/(auth)/login/page.jsx
+++ src/app/(auth)/login/page.jsx
@@ -1,21 +1,42 @@
+"use client"
+import Link from 'next/link';
 import React from 'react';
-export const metadata = {
-  title: 'Dragon News - login',
-}
+import { useForm } from 'react-hook-form';
+import toast, { Toaster } from 'react-hot-toast';
 
 const LoginPage = () => {
+    const {register,
+    handleSubmit,
+    formState: { errors },
+    } = useForm()
+    const handleLogin =(data) => {;
+        if (!data.email || !data.password) {
+            toast.error("Invalid Login");
+            return
+        }
+            toast.success("Login Successful")
+    }
     return (
-        <div className='max-w-3xl mx-auto'>
-            <form className="fieldset bg-base-200 border-base-300 rounded-box  border p-4 flex flex-col justify-center">
-                <h2 className='text-4xl font-bold'>Login your account</h2>
-  <label className="label">Email Address</label>
-  <input type="email" className="input" placeholder="Email" />
-
-  <label className="label">Password</label>
-  <input type="password" className="input" placeholder="Password" />
-
-  <button className="btn btn-neutral mt-4 w-80">Login</button>
-</form>
+        <div className=' bg-base-300'>
+            <div className='w-11/12 lg:w-10/12 mx-auto min-h-screen  flex justify-center items-center '>
+            <div className='bg-white p-10 rounded '>
+                <h2 className='font-bold text-3xl text-[#403F3F]'>Login Your Account</h2>
+                <form onSubmit={handleSubmit(handleLogin)} className="card-body">
+        <fieldset className="fieldset">
+          <label className="label font-semibold text-[#403F3F] text-lg">Email</label>
+          <input type="email" className="input bg-base-300" placeholder="Enter your email address" {...register("email", { required: "Email is Required" })}/>
+          {errors.email && <span className='text-red-500 mt-2'>{errors.email.message}</span>}
+          <label className="label font-semibold text-[#403F3F] text-lg">Password</label>
+          <input type="password" className="input bg-base-300" placeholder="Enter your password" {...register("password", { required: "You Can't Login Without Password" })}/>
+          {errors.password && <span className='text-red-500 mt-2'>{errors.password.message}</span>}
+          <div><a className="link link-hover">Forgot password?</a></div>
+          <button className="btn btn-neutral mt-4">Login</button>
+          <p className='text-[#403F3F] mt-3 font-semibold'>Don’t Have An Account ? <Link className='text-red-500' href='/register'>Register</Link></p>
+        </fieldset>
+      </form>
+            </div>
+        </div>
+        <Toaster></Toaster>
         </div>
     );
 };
--- src/app/(auth)/register/page.jsx
+++ src/app/(auth)/register/page.jsx
@@ -1,9 +1,45 @@
-import React from 'react';
+"use client"
+import { useForm } from 'react-hook-form';
+import toast, { Toaster } from 'react-hot-toast';
 
 const RegisterPage = () => {
+    const {register,
+    handleSubmit,
+    formState: { errors },
+    } = useForm()
+    const handleLogin =(data) => {;
+        if (!data.email || !data.password) {
+            toast.error("Invalid Login");
+            return
+        }
+            toast.success("Login Successful")
+    }
     return (
-        <div>
-            Register
+        <div className=' bg-base-300'>
+            <div className='w-11/12 lg:w-10/12 mx-auto min-h-screen  flex justify-center items-center '>
+            <div className='bg-white p-10 rounded '>
+                <h2 className='font-bold text-3xl text-[#403F3F]'>Register your account</h2>
+                <form onSubmit={handleSubmit(handleLogin)} className="card-body">
+        <fieldset className="fieldset">
+          <label className="label font-semibold text-[#403F3F] text-lg">Your Name</label>
+          <input type="text" className="input bg-base-300" placeholder="Your Full Name" {...register("name", { required: "Name Required" })}/>
+          {errors.name && <span className='text-red-500 mt-2'>{errors.name.message}</span>}
+          <label className="label font-semibold text-[#403F3F] text-lg">Photo URL</label>
+          <input type="text" className="input bg-base-300" placeholder="Enter Your Photo URL" {...register("photo", { required: "Upload Your Photo to Registration" })}/>
+          {errors.photo && <span className='text-red-500 mt-2'>{errors.photo.message}</span>}
+          <label className="label font-semibold text-[#403F3F] text-lg">Email</label>
+          <input type="email" className="input bg-base-300" placeholder="Enter your email address" {...register("email", { required: "Email is Required" })}/>
+          {errors.email && <span className='text-red-500 mt-2'>{errors.email.message}</span>}
+          <label className="label font-semibold text-[#403F3F] text-lg">Password</label>
+          <input type="password" className="input bg-base-300" placeholder="Enter your password" {...register("password", { required: "You Can't Login Without Password" })}/>
+          {errors.password && <span className='text-red-500 mt-2'>{errors.password.message}</span>}
+          <div><a className="link link-hover">Forgot password?</a></div>
+          <button className="btn btn-neutral mt-4">Register</button>
+        </fieldset>
+      </form>
+            </div>
+        </div>
+        <Toaster></Toaster>
         </div>
     );
 };
